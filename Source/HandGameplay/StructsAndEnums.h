20ab29ac40291ef8a469f10b22a9d1e8fc4fe7bf
Ciniaq\pmarc <pmarciniak000@gmail.com>
2023-08-21 19:17:43 +0200

    Added HandGestureEnum, added left and right hand variables, and setters for them

---
+++ Source/HandGameplay/StructsAndEnums.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EHandGestureEnum : uint8
+{
+	NotSet,
+	Victoria,
+	ClenchedHand,
+	AimingHand,
+	TwoFingerPistol,
+};
